cf1c92be81a25fad5f1d36420b2a5b0ed019c88a
Mahadev Jagtap <159867767+MahadevJagtap@users.noreply.github.com>
2025-09-30 22:20:23 +0530

    message

--- src/Components/partials/Sidenav.jsx
+++ src/Components/partials/Sidenav.jsx
@@ -1,45 +1,66 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidenav = () => {
   return (
-    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
-      <h1 className="text-2xl text-white font-bold flex flex-row">
-        <img src="/logo.svg" alt="SCSDB" className="w-5 mr-2" />
-        <span className="text-2xl">SCSDB</span>
+    <div className="w-[20%] h-full border-r-2 border-zinc-400 px-10 py-5">
+      <h1 className="text-3xl text-white font-semibold">
+        <i className="ri-tv-fill text-[#6556cd] mr-3"></i>
+        <span>SCSDB.</span>
       </h1>
 
-      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
-        <h1 className="text-white font-semibold text-xl mt-10 mb-5">
-          New Feeds
-        </h1>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-fire-fill mr-2"></i>
+      <nav className="flex flex-col text-xl gap-3 text-zinc-400">
+        <h1 className="text-xl text-white font-semibold mt-8 mb-3">New Feed</h1>
+        <Link
+          to="/trending"
+          className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2"
+        >
+          <i className="mr-2 ri-fire-fill"></i>
           Trending
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-bard-fill mr-2"></i>Popular
+        <Link
+          to="/popular"
+          className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2"
+        >
+          <i className="ri-bard-fill mr-2"></i>
+          Popular
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-movie-2-fill mr-2"></i>Movies
+        <Link
+          to="/movie"
+          className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2"
+        >
+          <i className="ri-movie-2-fill mr-2"></i>
+          Movies
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-tv-2-fill mr-2"></i>TV Shows
+        <Link
+          to="/tv"
+          className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2"
+        >
+          <i className="ri-tv-2-fill mr-2"></i>
+          Tv Shows
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-        <i className="ri-team-fill mr-2"></i>People</Link>
+        <Link
+          to="/person"
+          className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2"
+        >
+          <i className="ri-team-fill mr-2"></i>
+          People
+        </Link>
       </nav>
 
-      <hr className="border-none h-[1px] bg-zinc-400" />
-      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
-        <h1 className="text-white font-semibold text-xl mt-10 mb-5">
-          Website Information
+      <hr className="border-none h-[1px] bg-zinc-400 my-5" />
+
+      <nav className="flex flex-col text-xl gap-3 text-zinc-400">
+        <h1 className="text-xl text-white font-semibold my-2">
+          Website information
         </h1>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-information-fill mr-2"></i>
-          About SCSDB
+        <Link to="/about" className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2">
+          <i className="mr-2 ri-information-2-fill"></i>
+          About
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-md p-5">
-          <i className="ri-phone-fill mr-2"></i>Contact US
+        <Link to="/contact" className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg px-5 py-2">
+          <i className="mr-2 ri-phone-fill"></i>
+          Contact
         </Link>
       </nav>
     </div>
