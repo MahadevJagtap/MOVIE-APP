847693258401239954ab3c2f7a559afd2b1f514c
Mahadev Jagtap <159867767+MahadevJagtap@users.noreply.github.com>
2025-10-01 12:21:04 +0530

    message

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
-import Sidenav from './partials/Sidenav'
-import Topnav from './partials/Topnav';
+import React, { useEffect, useState } from "react";
+import Sidenav from "./partials/Sidenav";
+import Topnav from "./partials/Topnav";
+import Header from "./partials/Header";
+import axios from "../utils/axios";
+import Loader from '../Components/Loading'
 
+const Home = () => {
+  document.title = "SCSDB | Homepage";
+  const [wallpaper, setWallpaper] = useState(null);
+  const [trending, setTrending] = useState(null);
+  const [category, setCategory] = useState("all");
 
+  const getHeaderWallpaper = async () => {
+    try {
+      const { data } = await axios.get(`trending/all/week`);
+      let randomdata =
+        data.results[(Math.random() * data.results.length).toFixed()];
+      setWallpaper(randomdata);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
 
-function Home() {
-  return (
-  <>
-     <Sidenav/>
-      <div className="w-[80%] h-full bg-[#1F1E24]">
-      <Topnav />
+  const getTrending = async () => {
+    try {
+      const { data } = await axios.get(`trending/${category}/day`);
+      setTrending(data.results);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getTrending();
+    !wallpaper && getHeaderWallpaper();
+  }, [category]);
+
+  return wallpaper && trending ? (
+    <>
+      <Sidenav />
+      <div className="w-[80%] h-full overflow-auto overflow-x-hidden">
+        <Topnav />
+        <Header data={wallpaper} />
       </div>
-
+      
     </>
+  ) : (
+   <Loader />
   );
 };
 
-export default Home
+export default Home;
