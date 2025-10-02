0e9ac07d431023350f852a3120e580a80609ed2e
Mahadev Jagtap <159867767+MahadevJagtap@users.noreply.github.com>
2025-10-02 17:58:51 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,35 @@ import Popular from './Components/Popular';
 import Movie from './Components/Movie';
 import Tvshows from './Components/Tvshows';
 import People from './Components/People';
-
+import MovieDetails from './Components/MovieDetails';
+import Trailer from './Components/partials/Trailer';
+import TvDetails from './Components/TvDetails';
+import PersonDetails from './Components/PersonDetails';
+import NotFound from'../public/404.gif'
 
 
 function App() {
   return (
-     <div className="w-screen h-screen flex bg-[#1F1E24]">
+    <div className="w-screen h-screen bg-[#1f1e24] flex">
       <Routes>
-       <Route path='/' element={<Home />} />
-       <Route path='/trending' element={<Trending />} />
-       <Route path="/popular" element={<Popular />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/popular" element={<Popular />} />
         <Route path="/movie" element={<Movie />} />
+        <Route path="/movie/details/:id" element={<MovieDetails />}>
+          <Route path="/movie/details/:id/trailer" element={<Trailer />} />
+        </Route>
         <Route path="/tv" element={<Tvshows />} />
+        <Route path="/tv/details/:id" element={<TvDetails />}>
+          <Route path="/tv/details/:id/trailer" element={<Trailer />} />
+        </Route>
         <Route path="/person" element={<People />} />
-
-
-
-
-
+        <Route path="/person/details/:id" element={<PersonDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
     </div>
   );
 }
 
-export default App
+export default App;
